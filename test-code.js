eff0439b03527a63e5065b0f4009401fd7817491
dg-sbae <sbae@doctorgenius.com>
2021-02-16 00:23:52 -0800

    eslint configuration along with prettier

---
+++ test-code.js
@@ -0,0 +1,9 @@
+// // After installing, uncomment out this code and save the file - it should automatically reformat itself
+
+// function sayHi ( name  ){
+//   var age=10;
+// }
+
+// const age=12;
+
+// var things = ['single-quote', "double-quote"  ];
